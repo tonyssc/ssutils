6b4848c843f8d93916db84d3e9f19299cf59f9f9
sunshaocheng <sunsc.tony@gmail.com>
2018-06-11 20:39:19 +0800

    babel-loader

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -25,19 +25,19 @@ module.exports = {
                         loader: require.resolve('eslint-loader'),
                     },
                 ],
-                include: path.join(__dirname, 'src')
+                include: path.join(__dirname, '../src')
             },
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                include: path.join(__dirname, 'src')
+                include: path.join(__dirname, '../src')
             }
         ]
     },
     resolve: {
         modules: [ // 优化模块查找路径
-            path.join(__dirname, 'src'),
-            path.join(__dirname, 'node_modules') // 指定node_modules所在位置 当你import 第三方模块时 直接从这个路径下搜索寻找
+            path.join(__dirname, '../src'),
+            path.join(__dirname, '../node_modules') // 指定node_modules所在位置 当你import 第三方模块时 直接从这个路径下搜索寻找
         ],
         // 自动解析文件扩展名(补全文件后缀)(从左->右)
         // import hello from './hello'  （!hello.js? -> !hello.css? -> !hello.pug? -> !hello.json）
